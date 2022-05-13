dfda41b4f9a01a573e5fb7394c6e6cb6db52b2c2
Namana Khare <kharenamana@gmail.com>
2022-05-13 15:02:32 +0530

    Mobile Responsive

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import Dashboard from './components/dashboard/Dashboard';
 import Login from './components/Login';
-// import PrivateRoute from './components/PrivateRoute';
 import React from 'react';
 import { Route } from 'react-router-dom';
 import Signup from './components/Signup';
@@ -13,7 +12,7 @@ function App() {
     <AuthProvider>
       <Router>
         <Switch>
-          <Route exact path="/" component={Login} />0
+          <Route exact path="/" component={Login} />
           <Route path="/dashboard" component={Dashboard} />
           <Route path="/signup" component={Signup} />
         </Switch>
--- src/components/dashboard/Navbar.js
+++ src/components/dashboard/Navbar.js
@@ -22,11 +22,11 @@ const Navbar = (props) => {
   }
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand navbar-light bg-light">
       <Link className="navbar-brand" to="#">
         Calculator
       </Link>
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <div className="collapse  navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav mr-auto">
           <li className="nav-item active">
             <Link className="nav-link" to="#">
@@ -36,7 +36,7 @@ const Navbar = (props) => {
         </ul>
 
         <button
-          className="btn btn-outline-primary  my-2 my-sm-0"
+          className="btn btn-outline-primary  my-2 my-sm-1"
           variant="link"
           type="submit"
           onClick={() => handleLogout()}
